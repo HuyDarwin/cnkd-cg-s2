dbbec8952deb2c5df9c96b7cd9d016f19c389040
HuyDarwin <95059688+HuyDarwin@users.noreply.github.com>
2023-07-04 21:55:31 +0700

    Update sound_fb.js

--- JS/sound_fb.js
+++ JS/sound_fb.js
@@ -14,32 +14,32 @@ $(function () {
 			const data = snapshot.val();
 			
 			if (data.sound_intro == 1){
-				con.PlaySound('Sounds/Intro.mp3',1);
+				con.PlaySound('',1);
 				update(ref(db, 'commands'), { sound_intro : 0 });
 			}
 			if (data.sound_he == 1){
-				con.PlaySound('Sounds/Host Entrance.mp3',2);
+				con.PlaySound('',2);
 				update(ref(db, 'commands'), { sound_he : 0 });
 			}
 			
 			if (data.sound_1m == 1){
-				con.PlaySound('Sounds/1 Triệu CG.wav',2);
+				con.PlaySound('',2);
 				update(ref(db, 'commands'), { sound_1m : 0 });
 			}
 			if (data.sound_br == 1){
-				con.PlaySound('Sounds/Bankrupt.wav',2);
+				con.PlaySound('',2);
 				update(ref(db, 'commands'), { sound_br : 0 });
 			}
 			if (data.sound_ch == 1){
-				con.PlaySound('Sounds/Cơ Hội.wav',2);
+				con.PlaySound('',2);
 				update(ref(db, 'commands'), { sound_ch : 0 });
 			}
 			if (data.sound_jack == 1){
-				con.PlaySound('Sounds/Độc Đắc.wav',2);
+				con.PlaySound('',2);
 				update(ref(db, 'commands'), { sound_jack : 0 });
 			}
 			if (data.sound_dd == 1){
-				con.PlaySound('Sounds/Đổi Điểm.wav',2);
+				con.PlaySound('',2);
 				update(ref(db, 'commands'), { sound_dd : 0 });
 			}
 		})
